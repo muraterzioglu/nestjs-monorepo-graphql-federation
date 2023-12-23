528ac2cc3cdbdcecf67a59d42d576bb7ca9c6f69
Murat Terzioğlu <its.muraterzioglu@gmail.com>
2023-12-23 13:36:47 +0300

    ✨ Gateway App

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,7 +1,22 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'users', url: 'http://localhost:3001/graphql' },
+            { name: 'posts', url: 'http://localhost:3002/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
